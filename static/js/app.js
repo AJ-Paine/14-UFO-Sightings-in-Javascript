e9d9401e5163637c30a8d44daac751c7b5379867
AJ P <10paineaj@gmail.com>
2021-06-14 14:17:52 -0600

    Update to appened table data

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,27 @@
-// from data.js
-var tableData = data;
+// Grab data from data.js with new variable
+var sightings = data;
 
-// YOUR CODE HERE!
+//Select table body any existing table data
+var tableData = d3.select("table>tbody");
+
+//Clear exisiting data from table
+tableData.html("");
+
+//Append table data
+sightings.forEach((sighting) => {
+    var row = tableData.append("tr");
+    Object.entries(sighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value)
+    })
+});
+
+
+
+
+//var dateForm = d3.select(".filter");
+
+//Create event handler for date filter
+//dateForm.on("submit", runFilter);
+
+
